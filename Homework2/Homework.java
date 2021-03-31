f929000570a3bcef234e88775398a0fbdd009284
Ani Stepanyan <masriam.stepanyan@beeoncode.com>
2021-03-31 19:29:31 +0400

    first commit

--- Homework2/Homework.java
+++ Homework2/Homework.java
@@ -1,19 +1,22 @@
 public class Homework {
     public static void main(String[] args) {
         int a = 12, b = 25, c = 5, d = 234, e = 4;
-        System.out.println(a + b);
-        System.out.println(b - c);
-        System.out.println(a * b);
-        System.out.println(b / c);
-        System.out.println(a++ - --d);
-        System.out.println(++b * --c);
-        System.out.println(a % 2);
-        System.out.println(a == b || c > b);
-        System.out.println(a != b && c == e);
-        System.out.println(c <= d);
-        System.out.println(a & b);
-        System.out.println(d << 1);
-        System.out.println(a += b);
+        System.out.println("a + b : " + (a + b));
+        System.out.println("b - c : " + (b - c));
+        System.out.println("a * b : " + (a * b));
+        System.out.println("b / c : " + (b / c));
+        System.out.println("a++ - --d : " + (a++ - --d));
+        System.out.println("++b * --c : " + (++b * --c));
+        System.out.println("a % 2 : " + (a % 2));
+        System.out.println("a == b || c > b : " + (a == b || c > b));
+        System.out.println("a != b && c == e : " + (a != b && c == e));
+        System.out.println("c <= d : " + (c <= d));
+        System.out.println("a & b : " + (a & b));
+        System.out.println("c | a : " + (c | a));
+        System.out.println("c ^ b : " + (c ^ b));
+        System.out.println("c >>> e : " + (c >>> e));
+        System.out.println("d << 1 : "+(d << 1));
+        System.out.println("a = a + b : "+(a += b));
         int[] arr1 = new int[3];
         arr1[0] = 1;
         arr1[1] = 4;
--- Pordznakan/Test.java
+++
@@ -1,8 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        int a = 4;
-     if ((a&(a-1))==0){
-         System.out.println("true");
-     }
-    }
-}
